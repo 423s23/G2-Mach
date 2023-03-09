2000e6a943f47b3c4d70966e69ab95374f8d5d11
Josh-Fried <98046392+Josh-Fried@users.noreply.github.com>
2023-03-09 16:15:35 -0700

    Adding Points to TaskList Screen

--- MachProject/components/NavBar.jsx
+++ MachProject/components/NavBar.jsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
 
-import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons'
 
@@ -11,9 +9,6 @@ import Home from '../screens/Home'
 import Leaderboard from '../screens/Leaderboard'
 import Profile from '../screens/Profile'
 import Tasks from '../screens/TaskList'
-import {navigationRef} from "../RootNavigation";
-import StackNavigator from "@react-navigation/stack/src/navigators/createStackNavigator";
-import {Screen} from "react-native-screens";
 
 //Screen names
 const homeName = 'Home'
