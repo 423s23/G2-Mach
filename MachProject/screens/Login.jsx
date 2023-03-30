14b5bb377abe2ce96b090b3223825be3aa072a6f
Brady Ash <bradyash9@gmail.com>
2023-03-30 16:06:20 -0600

    Login.jsx created and begun styling

--- MachProject/screens/Login.jsx
+++ MachProject/screens/Login.jsx
@@ -25,7 +25,7 @@ export default function App() {
             <View style={styles.inputView}>
                 <TextInput
                     style={styles.TextInput}
-                    placeholder="Email."
+                    placeholder="Email"
                     placeholderTextColor="#003f5c"
                     onChangeText={(email) => setEmail(email)}
                 />
@@ -33,7 +33,7 @@ export default function App() {
             <View style={styles.inputView}>
                 <TextInput
                     style={styles.TextInput}
-                    placeholder="Password."
+                    placeholder="Password"
                     placeholderTextColor="#003f5c"
                     secureTextEntry={true}
                     onChangeText={(password) => setPassword(password)}
