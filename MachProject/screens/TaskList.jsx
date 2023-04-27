b5f7f783c22406b9c2c143c4f069838aa2900c61
Josh-Fried <98046392+Josh-Fried@users.noreply.github.com>
2023-04-27 16:57:05 -0600

    More Tasks

--- MachProject/screens/TaskList.jsx
+++ MachProject/screens/TaskList.jsx
@@ -8,11 +8,16 @@ import {withNavigation} from "react-navigation";
 
 const TaskList = ({ navigation }) => {
     let duties = [
-        { id: 1, title: 'Write a Review', points: 5, description: 'Jersey Review = 5 points'+'\nBib Review = 5 points'+'\nTri-suit Review = 5 points'+'\nTotal Points = 15 points' },
-        { id: 2, title: 'Race in Mach Kit', points: 5, description: 'Everytime you race in your kit you receive 5 points.' + '\nLet the Athlete Director know when/where you raced (photos encouraged for more points!).'},
-        { id: 3, title: 'ZSwift Mach Handle', points: 10, description: ''},
-        { id: 4, title: 'Place Handle in Bio', points: 10, description: '@machappareltri' + '\nLet us know if you place our instagram handle in your bio and you’ll receive points.' + '\nFor every new calendar year you’ll receive 10 points.'},
-        { id: 5, title: 'Send Us A Quality Photo', points: 10, description: 'MACH Rights' + '\nCollaborate via instagram or send us your quality training photos'},
+        { id: 1, title: 'Write a Jersey Review', points: 5, description: 'Write a Jersey review on machapparel.com' },
+        {id: 2, title: 'Write a Bib Review',points: 5,description: "Write a Bib review on machapparel.com"},
+        {id: 3, title: 'Write a Tri-Suit Review',points: 5,description: "Write a Tri-Suit review on machapparel.com"},
+        { id: 4, title: 'Race in Mach Kit', points: 5, description: 'Everytime you race in your kit you receive 5 points.' + '\nLet the Athlete Director know when/where you raced (photos encouraged for more points!).'},
+        { id: 5, title: 'Place Handle in Bio', points: 10, description: '@machappareltri' + '\nLet us know if you place our instagram handle in your bio and you’ll receive points.' + '\nFor every new calendar year you’ll receive 10 points.'},
+        { id: 6, title: 'Send Us A Quality Photo', points: 10, description: 'MACH Rights' + '\nCollaborate via instagram or send us your quality training photos'},
+        { id: 7, title: 'Race Finish Photo', points: 20, description: 'MACH Rights' + '\nCollaborate via instagram or send us your race photos.'},
+        { id: 8, title: 'Create A Quality Post', points: 10, description: 'MACH Rights' + '\nCollaborate with us via instagram.' + '\nImage needs to be professional quality.'},
+        { id: 9, title: 'Create A Quality Tiktok', points: 10, description: 'MACH Rights' + '\nCollaborate with us via instagram/TikTok.' + '\nVideo needs to be professional quality.'},
+        { id: 10, title: 'Order Team Bundle', points: 200, description: 'From machapparel.com' + '\nTeam bundle includes ordering team tri-suit, t-shirt, socks, hat.'},
     ];
 
     const handlePress = (duty) => { // Goes to Task page (Tasks.jsx)
